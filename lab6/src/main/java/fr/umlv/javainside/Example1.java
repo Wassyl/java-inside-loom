c7fa0b714063ca5aca9ff4879dfc16555e669c68
EL HACHIMI Wassyl <wassyl.elhachimi@gmail.com>
2020-10-27 11:38:40 +0100

    Continuations

--- lab6/src/main/java/fr/umlv/javainside/Example1.java
+++ lab6/src/main/java/fr/umlv/javainside/Example1.java
@@ -4,28 +4,30 @@ import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Example1 {
-    public static void main(String[] args) {
+    public static void continuationTest(String[] args) {
         var lock = new Object();
         var lockRe = new ReentrantLock();
 
-        var scope = new ContinuationScope("hello1");
-        var cont = new Continuation( scope, () -> {
+        var oldScope = new ContinuationScope("hello1");
+        var cont = new Continuation( oldScope, () -> {
 
             //Continuation.yield(scope);
             System.out.println("Hello continuation");
             /*
+            // Synchronized est fait en assembleur : difficile à update
             synchronized(lockRe) {
                 Continuation.yield(scope);
             }
             */
+            // ReentrantLock est fait en Java : OK !
             lockRe.lock();
             try{
-                Continuation.yield(scope);
+                Continuation.yield(oldScope);
             } finally {
                 lockRe.unlock();
             }
 
-            var test = Continuation.getCurrentContinuation(scope);
+            var test = Continuation.getCurrentContinuation(oldScope);
             System.out.println("Hello continuation");
 
         });
@@ -36,7 +38,7 @@ public class Example1 {
         */
 
 
-        var newScope = new ContinuationScope("scope");
+        var scope = new ContinuationScope("scope");
         var continuation1 = new Continuation(scope, () -> {
             System.out.println("start 1");
             Continuation.yield(scope);
@@ -64,5 +66,27 @@ public class Example1 {
             list.forEach( Continuation::run );
         }
 
+    }
+
+    public static void main(String[] args) {
+        var scope = new ContinuationScope("scope");
+        var scheduler = new Scheduler();
+        var continuation1 = new Continuation(scope, () -> {
+            System.out.println("start 1");
+            scheduler.enqueue(scope);
+            System.out.println("middle 1");
+            scheduler.enqueue(scope);
+            System.out.println("end 1");
+        });
+        var continuation2 = new Continuation(scope, () -> {
+            System.out.println("start 2");
+            scheduler.enqueue(scope);
+            System.out.println("middle 2");
+            scheduler.enqueue(scope);
+            System.out.println("end 2");
+        });
+        var list = List.of(continuation1, continuation2);
+        list.forEach(Continuation::run);
+        scheduler.runLoop();
     }
 }
--- lab6/src/main/java/fr/umlv/javainside/Scheduler.java
+++ lab6/src/main/java/fr/umlv/javainside/Scheduler.java
@@ -1,4 +1,47 @@
 package fr.umlv.javainside;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class Scheduler {
+    private enum SCHEDULER_POLICY{
+        STACK,FIFO,RANDOM
+    }
+    private SCHEDULER_POLICY policy = SCHEDULER_POLICY.STACK;
+
+    private final ArrayList<Continuation> list = new ArrayList();
+
+    public void enqueue( ContinuationScope scope ) {
+        var cont = Continuation.getCurrentContinuation(scope);
+        if( cont == null ){
+            throw new IllegalStateException("No running continuation");
+        }
+        list.add(cont);
+        Continuation.yield(scope);
+    }
+
+    public void runLoop() {
+        while( !list.stream().allMatch(Continuation::isDone) ){
+            switch ( policy ){
+                case STACK:{
+                    var conts = list.stream()
+                                                    .filter( e -> !e.isDone() )
+                                                    .collect(Collectors.toList());
+                    var cont = conts.get( conts.size()-1 );
+                    cont.run();
+                }
+                case FIFO: {
+
+                }
+                case RANDOM:{
+                    var rand = new Random();
+                    var cont = list.get( rand.nextInt(list.size()) );
+                    cont.run();
+                }
+            }
+
+        }
+    }
+
 }
